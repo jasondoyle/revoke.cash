5aca7042ec3bdecfa2cb51d3d0de8660f78868c3
Rosco Kalis <roscokalis@gmail.com>
2023-05-07 22:40:24 +0200

    Use typescript strict mode

--- components/common/ChainSelect.tsx
+++ components/common/ChainSelect.tsx
@@ -79,6 +79,7 @@ const ChainSelect = ({ onSelect, selected, showName, menuAlign, chainIds }: Prop
       minMenuWidth="13.5rem"
       placeholder={<PlaceholderIcon size={24} border />}
       menuAlign={menuAlign}
+      isMulti={false}
     />
   );
 };
